a7e8472be2f005d8262601a9ddc5929c7e275f8f
heinthant <hthant00chk@gmail.com>
2023-02-18 20:37:47 +0630

    Add basic chessboard with validations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import ChessBoard from 'chessboardjsx';
+import ChessBoard from './features/chessboard/Chessboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <ChessBoard position="start" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChessBoard />
     </div>
   );
 }
